141d70e0e033b0dac8fa14305283726321e31d92
PereHDZ <pere.hdz.salleras@gmail.com>
2025-03-21 19:14:00 +0100

    Final commit

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allow all origins (change this to specific domains if needed)
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
