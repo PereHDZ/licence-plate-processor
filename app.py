62100d7879446940bd6fd5a28248c0bb78c3861b
PereHDZ <pere.hdz.salleras@gmail.com>
2025-03-21 12:04:28 +0100

    .

--- app.py
+++ app.py
@@ -2,7 +2,12 @@ import streamlit as st
 import requests
 from PIL import Image
 from io import BytesIO
+import warnings
+import logging
 
+# Suppress Streamlit and other warnings from being displayed on the app
+logging.getLogger('streamlit').setLevel(logging.ERROR)  # Set Streamlit logger to show only errors
+warnings.filterwarnings("ignore")  # Suppress other warnings
 
 # URL of the FastAPI backend
 API_URL = "https://licence-plate-processor.onrender.com/predict/"
@@ -16,7 +21,8 @@ uploaded_file = st.file_uploader("Upload an Image of a License Plate", type=["jp
 if uploaded_file is not None:
     # Open the uploaded image
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    # Use `use_container_width` instead of the deprecated `use_column_width`
+    st.image(image, caption="Uploaded Image", use_container_width=True)
     
     # Convert image to bytes
     img_byte_arr = BytesIO()
